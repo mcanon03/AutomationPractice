786014affbed516de75314bfad3d0264c1a2bc49
Marilou Canon <marilou@dialexa.com>
2016-12-14 11:35:23 -0600

    First commit

--- tests/searchTestGoogle.js
+++ tests/searchTestGoogle.js
@@ -3,8 +3,6 @@ module.exports = {
     browser
       .url('http://www.google.com')
       .waitForElementVisible('body', 1000)
-      //.assert.title('Google')
-      //.assert.visible('input[type=text]')
       .setValue('input[type=text]', 'crossfit214')
       .waitForElementVisible('button[name=btnG]', 1000)
       .click('button[name=btnG]')

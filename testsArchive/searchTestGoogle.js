bf5dc4d1ff9d0e56c2f96cca030ee863ba1cfa80
Marilou Canon <marilou@dialexa.com>
2016-12-20 00:53:39 -0600

    Can select # of passengers

--- testsArchive/searchTestGoogle.js
+++ testsArchive/searchTestGoogle.js
@@ -1,3 +1,5 @@
+'use strict'
+
 module.exports = {
   'Search Test Google' : function (browser) {
     browser
--- testsArchive/searchTestNPM.js
+++ testsArchive/searchTestNPM.js
@@ -1,3 +1,5 @@
+'use strict'
+
 module.exports = {
   'Search Test NPM' : function (browser) {
     browser
